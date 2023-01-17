9541a74f3efaf45e2014e1a9d470517b725e89e4
c_davidson <c.monee.davidson@gmail.com>
2023-01-16 21:58:04 -0600

    Revised typo/spacing

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ const gitUserInfo = async function () {
     gitRepos();
 };
 
-const gitRepos = async function () {
+const gitRepos = async function (username) {
   const fetchRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
   const repoData = await fetchRepos.json();
   displayRepos(repoData);
